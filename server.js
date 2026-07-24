672611de9e4023a2fd7e921a85a4cf136eb3fd9f
Jo4oPaulo <joaopaulosousa.jpsds@gmail.com>
2026-07-24 18:01:01 -0300

    fix: ajustando estrutura do projeto

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-import express, { response } from "express";
+import express from "express";
 import prismaClient from "./database/PrismaClient.js";
 import router from "./src/routes/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(router)
-
-app.get("/players", async (request, response) => {
-    const players = await prismaClient.player.findMany();
-    return response.status(200).json(players);
-})
 
 app.listen(3000, () => {
     console.log("Server running")
